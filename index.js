862ec43b9280e4e35f02843dfd3f142c3451a072
idob <idobakimer@gmail.com>
2022-07-10 12:33:46 +0300

    proto gravity

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
 class Player {
     constructor() {
     this.position = {
         x: 100,
         y: 100
     }
-    this.width = 100
-    this.height = 100
+    this.velocity ={
+        x:0,
+        y:1
+    }
+    this.width = 30
+    this.height = 30
     }
     draw(){
+        c.fillStyle = 'red'
         c.fillRect(this.position.x,this.position.y,this.width,this.height)
     }
+
+update(){
+    this.position.y += this.velocity.y
+    this.draw()
+}
+
 }
 
 
 const player = new Player()
-player.draw()
+
+function animate (){
+    requestAnimationFrame(animate)
+    player.update()
+}
+
+animate()
+
+
