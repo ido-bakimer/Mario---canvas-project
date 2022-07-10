1f41386108921ef4966e0025f872e53846667d82
idob <idobakimer@gmail.com>
2022-07-10 12:49:49 +0300

    keys event listener

--- index.js
+++ index.js
@@ -7,41 +7,42 @@ canvas.height = window.innerHeight
 const gravity = 0.5
 class Player {
     constructor() {
-    this.position = {
-        x: 100,
-        y: 100
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: -10
+        }
+        this.width = 30
+        this.height = 30
     }
-    this.velocity ={
-        x:0,
-        y:-10
-    }
-    this.width = 30
-    this.height = 30
-    }
-    draw(){
+    draw() {
         c.fillStyle = 'red'
-        c.fillRect(this.position.x,this.position.y,this.width,this.height)
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 
-update(){
-    this.position.y += this.velocity.y
-    this.draw()
-    if(this.position.y + this.height + this.velocity.y <= canvas.height)
-    {this.velocity.y += gravity}
-    else {this.velocity.y = 0}
-}
+    update() {
+        this.position.y += this.velocity.y
+        this.draw()
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) { this.velocity.y += gravity }
+        else { this.velocity.y = 0 }
+    }
 
 }
 
 
 const player = new Player()
 
-function animate (){
+function animate() {
     requestAnimationFrame(animate)
-    c.clearRect(0,0,canvas.width,canvas.height)
+    c.clearRect(0, 0, canvas.width, canvas.height)
     player.update()
 }
 
 animate()
 
-
+window.addEventListener('keydown', (event) =>{
+    console.log(event)
+})
